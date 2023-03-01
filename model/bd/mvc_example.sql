44a1333df18c6e75bfc1f81a47e9f4c91ed495c5
Yulieth Collazos <yulicollazos20@gmail.com>
2023-02-28 19:02:50 -0500

    database

--- model/bd/mvc_example.sql
+++ model/bd/mvc_example.sql
@@ -1,11 +1,6 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 8
 -- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 21-02-2023 a las 03:34:20
--- Versión del servidor: 10.4.25-MariaDB
--- Versión de PHP: 7.4.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
